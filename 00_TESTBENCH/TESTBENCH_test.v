18f1f9454183cafd8891609bdc10637a4b070e16
Gene <eugeneleeeutzuan@gmail.com>
2016-09-04 12:26:23 +0800

    typo

--- 00_TESTBENCH/TESTBENCH_test.v
+++ 00_TESTBENCH/TESTBENCH_test.v
@@ -35,7 +35,7 @@ wire  [15:0]  out_data;
 reg [8:0] test1, test2;
 reg signed [9:0] test3;
 
-module sign_test(
+sign_test u_sign_test(
   .sign1    (test1),
   .sign2    (test2),
   .sign3    (test3)
