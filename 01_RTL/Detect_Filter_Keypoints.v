e7d66c10371aaa53ba28da7b1001fcef26003f77
Gene <eugeneleeeutzuan@gmail.com>
2016-09-10 18:40:08 +0800

    paranthesis and statement fix

--- 01_RTL/Detect_Filter_Keypoints.v
+++ 01_RTL/Detect_Filter_Keypoints.v
@@ -223,7 +223,7 @@ always @(posedge clk) begin
   if (!rst_n) 
     current_col <= 'd1;    
   else if (( (current_state==ST_FILTER && filter_count==(keypoint_count-1)) ||
-             (current_state==ST_NO_FILTER ||
+              current_state==ST_NO_FILTER ||
              (current_state==ST_DETECT && !(|is_keypoint))) && current_col < 'd639) /*if no keypoints found*/
     current_col <= current_col + 1;
   else if (current_state==ST_UPDATE || current_state==ST_IDLE)
@@ -374,7 +374,7 @@ always @(*) begin
         next_state = ST_DETECT;
     end
     ST_NO_FILTER: begin
-      else if(current_col == 'd639)
+      if(current_col == 'd639)
         next_state = ST_UPDATE;
       else if(current_col < 'd639)
         next_state = ST_DETECT;
