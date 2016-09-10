a4dda2bdb2a5b1747119ee37f0fb047e3dfe0de4
Gene <eugeneleeeutzuan@gmail.com>
2016-09-10 19:40:46 +0800

    turn fitler on

--- 00_TESTBENCH/TESTBENCH.v
+++ 00_TESTBENCH/TESTBENCH.v
@@ -67,7 +67,7 @@ initial begin
   rst_n     = 1;
   start  = 0;
   imageFile  = $fopen("originalImage.txt","r");
-  filter_on = 0; /*Turns filter on*/
+  filter_on = 1; /*Turns filter on*/
   filter_threshold = 6; /*Sets fitler threshold*/
 
   for(i=0;i<`ROWS;i=i+1) begin
