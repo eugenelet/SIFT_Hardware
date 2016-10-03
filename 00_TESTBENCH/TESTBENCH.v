eba7068cb088ed764dbb53812ddac3ca277093aa
Gene <eugeneleeeutzuan@gmail.com>
2016-10-03 14:10:20 +0800

    add writable file

--- 00_TESTBENCH/TESTBENCH.v
+++ 00_TESTBENCH/TESTBENCH.v
@@ -132,6 +132,7 @@ integer targetKptNum;
 integer temp;
 integer cycleCount, detectCount, filterCount, kp_count;
 integer match;
+integer matched_pairs;
 initial begin
   rst_n             = 1;
   start             = 0;
@@ -712,6 +713,7 @@ initial begin
    // ansFile = $fscanf("")
    // rc = $fscanf(ansFile, "%d", match_succeed_num_ANS);
    // $display("Ans matched num : %d", match_succeed_num_ANS);
+   matched_pairs = $fopen("matched_pairs.txt", "w");
    matched_addr2_in = 0;
    @(negedge clk);
    matched_addr2_in = 1;
@@ -721,19 +723,26 @@ initial begin
      temp = i & 2'b11;
      if(temp[1:0] == 2'b00) begin
         matched_mem_0[i / 4] = matched_0_dout;
+        $fwrite(matched_pairs, "0 %d %d\n", matched_mem_0[29:15], matched_mem_0[14:0]);
      end
      else if(temp[1:0] == 2'b01) begin
         matched_mem_1[i / 4] = matched_1_dout;
+        $fwrite(matched_pairs, "0 %d %d\n", matched_mem_0[29:15], matched_mem_0[14:0]);
+
      end
      else if(temp[1:0] == 2'b10) begin
         matched_mem_2[i / 4] = matched_2_dout;
+        $fwrite(matched_pairs, "0 %d %d\n", matched_mem_0[29:15], matched_mem_0[14:0]);
      end
      else begin
         matched_mem_3[i / 4] = matched_3_dout;
+        $fwrite(matched_pairs, "0 %d %d\n", matched_mem_0[29:15], matched_mem_0[14:0]);
         matched_addr2_in = matched_addr2_in + 1;
         @(negedge clk);
      end
    end
+
+   $fclose(matched_pairs);
 
    for(i = 0; i < targetKptNum; i = i + 1) begin
        temp = i & 2'b11;
